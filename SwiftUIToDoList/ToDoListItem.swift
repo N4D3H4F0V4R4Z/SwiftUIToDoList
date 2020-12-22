3c8a891c9696789168ed40687b96dd7c9bc074fc
Araz Nadzhafov <brainless11.5.1994@gmail.com>
2020-12-23 01:33:42 +0300

    SwiftUI

---
+++ SwiftUIToDoList/ToDoListItem.swift
@@ -0,0 +1,28 @@
+//
+//  ToDoListItem.swift
+//  SwiftUIToDoList
+//
+//  Created by Наджафов Араз on 22.12.2020.
+//
+
+import Foundation
+import CoreData
+
+class ToDoListItem: NSManagedObject, Identifiable {
+    @NSManaged var name: String?
+    @NSManaged var createdAt: Date?
+}
+
+extension ToDoListItem {
+    
+    static func getAllToDoListItems() -> NSFetchRequest<ToDoListItem> {
+        
+        let request: NSFetchRequest<ToDoListItem> = ToDoListItem.fetchRequest() as! NSFetchRequest<ToDoListItem>
+        let sort = NSSortDescriptor(key: "createdAt", ascending: true)
+        request.sortDescriptors = [sort]
+        
+        return request
+        
+    }
+    
+}
